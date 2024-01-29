33911929039f62301d93adf9c41c5dda2c692554
joelalves <joel.17.alves@gmail.com>
2024-01-29 17:23:57 +0000

    new deploy 2024-1-29  17:23

---
+++ _next/static/_GANI9NkZ8QRVO5fsKmla/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/262-23e559a4750ee96a.js","static/chunks/pages/index-20d7ff14a735af61.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
